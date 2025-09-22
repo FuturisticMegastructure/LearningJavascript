436f0f2ae2fba274170426bdd33b4802bca3c91b
Oswaldo Trinidad <abraham.trinidad@proton.me>
2025-09-21 21:56:42 -0600

    Added a header and a function to change its content

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,10 @@ function toggleDone(e) {
 listItems.forEach((item) => {
   item.addEventListener("click", toggleDone);
 });
+
+
+
+// Store a reference to the <h1> in a variable
+const myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
+myHeading.textContent = "Hello world!";
