62d12ad4561e4020642b836bec866154b505d2bf
Oswaldo Trinidad <abraham.trinidad@proton.me>
2025-09-23 16:31:36 -0600

    Added Autopoints functionality, not working

--- scripts/lessonsScripts/Lesson2/variables.js
+++ scripts/lessonsScripts/Lesson2/variables.js
@@ -6,6 +6,8 @@ const buttonMultiplier = document.querySelector("#button_multiplier");
 const countMultiplier = document.querySelector("#text_multiplier");
 const buttonPercent = document.querySelector("#button_percent");
 const countPercent = document.querySelector("#text_percent");
+const buttonAutoPoints = document.querySelector("#button_auto_points");
+const countAutoPoints = document.querySelector("#text_auto_points");
 
 let totalPoints = 0;
 let extraPoints = 0;
@@ -14,10 +16,17 @@ let totalMultiplier = 1;
 let costMultiplier = 100;
 let totalPercent = 1;
 let costPercent = 1000;
+let autoPointsMultiplier = 0;
+let autoPoints = extraPoints*autoPointsMultiplier;
+let costAutoPoints = 0;
+
+function updatePoints(){
+    textPoints.textContent = `You have ${totalPoints.toFixed(1)} Points right now`;
+}
 
 buttonPoints.onclick = () => {
     totalPoints = totalPoints+((1+extraPoints)*(totalMultiplier*totalPercent));
-    textPoints.textContent = `You have ${totalPoints.toFixed(1)} Points right now`;
+    updatePoints();
 };
 
 buttonExtraPoints.onclick = () => {
@@ -25,7 +34,7 @@ buttonExtraPoints.onclick = () => {
         extraPoints += 1;
         totalPoints -= costExtraBasePoints;
         costExtraBasePoints += 1;
-        textPoints.textContent = `You have ${totalPoints.toFixed(1)} Points right now`
+        updatePoints();
         countExtraBasePoints.textContent = `You make ${extraPoints.toFixed(1)} Extra Points per click, next upgrade costs ${costExtraBasePoints}`;
     }
 };
@@ -35,7 +44,7 @@ buttonMultiplier.onclick = () => {
         totalMultiplier += 0.1;
         totalPoints -= costMultiplier;
         costMultiplier += 100;
-        textPoints.textContent = `You have ${totalPoints.toFixed(1)} Points right now`
+        updatePoints();
         countMultiplier.textContent = `You have x${totalMultiplier.toFixed(1)} multiplier, next upgrade costs ${costMultiplier}`;
     }
 };
@@ -45,7 +54,22 @@ buttonPercent.onclick = () => {
         totalPercent += 0.5;
         totalPoints -= costPercent;
         costPercent *= 1.5;
-        textPoints.textContent = `You have ${totalPoints.toFixed(1)} Points right now`
+        updatePoints();
         countPercent.textContent = `You have x${totalPercent.toFixed(1)} percent multiplier, next upgrade costs ${costPercent.toFixed(1)}`;
     }
 };
+
+buttonAutoPoints.onclick = () => {
+    if(totalPoints>=costAutoPoints){
+        autoPointsMultiplier = (autoPointsMultiplier+.01)*1.1;
+        totalPoints -= costAutoPoints;
+        costAutoPoints = costAutoPoints*1.1;
+        updatePoints();
+        countAutoPoints.textContent = `You make ${(autoPoints*2).toFixed(1)} percentil Auto Points pe second , next upgrade costs ${costAutoPoints.toFixed(1)}`;
+    }
+};
+
+setInterval(() => {
+    totalPoints += autoPoints;
+    updatePoints();
+}, 500);
