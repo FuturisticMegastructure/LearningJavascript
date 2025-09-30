e6d0f3e1fe5c773f921255def48057ec56855026
Oswaldo Trinidad <abraham.trinidad@proton.me>
2025-09-30 12:13:37 -0600

    Changed the whole interface to match the IdleSphereProgram prototype, fixed issues with visualization

--- scripts/lessonsScripts/Lesson2/variables.js
+++ scripts/lessonsScripts/Lesson2/variables.js
@@ -1,75 +1,129 @@
-const buttonPoints = document.querySelector("#button_points");
-const textPoints = document.querySelector("#text_points");
-const buttonExtraPoints = document.querySelector("#button_extra_points");
-const countExtraBasePoints = document.querySelector("#text_extra_points");
-const buttonMultiplier = document.querySelector("#button_multiplier");
-const countMultiplier = document.querySelector("#text_multiplier");
-const buttonPercent = document.querySelector("#button_percent");
-const countPercent = document.querySelector("#text_percent");
-const buttonAutoPoints = document.querySelector("#button_auto_points");
-const countAutoPoints = document.querySelector("#text_auto_points");
+let resources = document.querySelector('.resources-count')
+let parsedResources = parseFloat(resources.innerHTML)
+let science = document.querySelector('.science-count')
+let parsedScience = parseFloat(science.innerHTML)
+let resourcesPerClick = 1
+let resourcesPerSecond = 0
+let sciencePerClick = 1
+let sciencePerSecond = 0
 
-let totalPoints = 0;
-let extraPoints = 0;
-let costExtraBasePoints = 10;
-let totalMultiplier = 1;
-let costMultiplier = 100;
-let totalPercent = 1;
-let costPercent = 1000;
-let autoPointsMultiplier = 0;
-let autoPoints = extraPoints*autoPointsMultiplier;
-let costAutoPoints = 0;
+let rps = document.getElementById("resources-per-second-text")
+let sps = document.getElementById("science-per-second-text")
+let randspc = document.getElementById("rands-per-clic-text")
 
-function updatePoints(){
-    textPoints.textContent = `You have ${totalPoints.toFixed(1)} Points right now`;
+let willpowerResourcesCost = document.querySelector('.willpower-resources-cost')
+let parsedWillpowerResourcesCost = parseFloat(willpowerResourcesCost.innerHTML)
+let willpowerScienceCost = document.querySelector('.willpower-science-cost')
+let parsedWillpowerScienceCost = parseFloat(willpowerScienceCost.innerHTML)
+let willpowerCount= document.querySelector(".willpower-count")
+let parsedWillpowerCount = parseFloat(willpowerCount.innerHTML)
+let willpowerIncrease = document.querySelector(".willpower-increase")
+let parsedWillpowerIncrease = parseFloat(willpowerIncrease.innerHTML)
+
+let workerCost = document.querySelector('.worker-cost')
+let parsedWorkerCost = parseFloat(workerCost.innerHTML)
+let workerCount= document.querySelector(".worker-count")
+let parsedWorkerCount = parseFloat(workerCount.innerHTML)
+let workerIncrease = document.querySelector(".worker-increase")
+let parsedWorkerIncrease = parseFloat(workerIncrease.innerHTML)
+
+let researcherCost = document.querySelector('.researcher-cost')
+let parsedResearcherCost = parseFloat(researcherCost.innerHTML)
+let researcherCount= document.querySelector(".researcher-count")
+let parsedResearcherCount = parseFloat(researcherCount.innerHTML)
+let researcherIncrease = document.querySelector(".researcher-increase")
+let parsedResearcherIncrease = parseFloat(researcherIncrease.innerHTML)
+
+let resourceImageContainer = document.querySelector(".resource-image-container")
+let scienceImageContainer = document.querySelector(".science-image-container")
+
+const timeout = (div) => {
+    setTimeout(() =>{
+        div.remove()
+    },900)
 }
 
-buttonPoints.onclick = () => {
-    totalPoints = totalPoints+((1+extraPoints)*(totalMultiplier*totalPercent));
-    updatePoints();
-};
+function incrementResources(event){
+    resources.innerHTML = Math.round(parsedResources += (resourcesPerClick))
 
-buttonExtraPoints.onclick = () => {
-    if(totalPoints>=costExtraBasePoints){
-        extraPoints += 1;
-        totalPoints -= costExtraBasePoints;
-        costExtraBasePoints += 1;
-        updatePoints();
-        countExtraBasePoints.textContent = `You make ${extraPoints.toFixed(1)} Extra Points per click, next upgrade costs ${costExtraBasePoints}`;
+    const x = event.offsetX
+    const y = event.offsetY
+
+    const div = document.createElement("div")
+    div.innerHTML = `+${Math.round(resourcesPerClick)}`
+    div.style.cssText = `color: white; position: absolute; top: ${y}px; left: ${x}px; font-size: 15px; pointer-events: none;`
+    resourceImageContainer.appendChild(div)
+
+    div.classList.add("fade-up")
+
+    timeout(div)
+}
+
+function incrementScience(event){
+    science.innerHTML = Math.round(parsedScience += (sciencePerClick))
+
+    const x = event.offsetX
+    const y = event.offsetY
+
+    const div = document.createElement("div")
+    div.innerHTML = `+${Math.round(sciencePerClick)}`
+    div.style.cssText = `color: white; position: absolute; top: ${y}px; left: ${x}px; font-size: 15px; pointer-events: none;`
+    scienceImageContainer.appendChild(div)
+
+    div.classList.add("fade-up")
+
+    timeout(div)
+}
+
+function buyWillpower(){
+    if (parsedResources >= parsedWillpowerResourcesCost && parsedScience >= parsedWillpowerScienceCost){
+        resources.innerHTML = Math.round(parsedResources -= parsedWillpowerResourcesCost)
+        science.innerHTML = Math.round(parsedScience -= parsedWillpowerScienceCost)
+        willpowerCount.innerHTML ++
+
+        parsedWillpowerIncrease = parseFloat((parsedWillpowerIncrease * 1.05).toFixed(2))
+        willpowerIncrease.innerHTML = parsedWillpowerIncrease
+        parsedWillpowerResourcesCost *= 1.1;
+        willpowerResourcesCost.innerHTML = Math.round(parsedWillpowerResourcesCost)
+        parsedWillpowerScienceCost *= 1.1;
+        willpowerScienceCost.innerHTML = Math.round(parsedWillpowerScienceCost)
+        resourcesPerClick = 1 + parsedWillpowerIncrease
+        sciencePerClick = 1 + parsedWillpowerIncrease
     }
-};
+}
 
-buttonMultiplier.onclick = () => {
-    if(totalPoints>=costMultiplier){
-        totalMultiplier += 0.1;
-        totalPoints -= costMultiplier;
-        costMultiplier += 100;
-        updatePoints();
-        countMultiplier.textContent = `You have x${totalMultiplier.toFixed(1)} multiplier, next upgrade costs ${costMultiplier}`;
+function buyWorker(){
+    if (parsedResources >= parsedWorkerCost){
+        resources.innerHTML = Math.round(parsedResources -= parsedWorkerCost)
+        workerCount.innerHTML ++
+
+        parsedWorkerIncrease = parseFloat((workerCount.innerHTML * 1).toFixed(2))
+        workerIncrease.innerHTML = parsedWorkerIncrease
+        parsedWorkerCost *= 1.2;
+        workerCost.innerHTML = Math.round(parsedWorkerCost)
+        resourcesPerSecond = 0 + parsedWorkerIncrease
     }
-};
+}
 
-buttonPercent.onclick = () => {
-    if(totalPoints>=costPercent){
-        totalPercent += 0.5;
-        totalPoints -= costPercent;
-        costPercent *= 1.5;
-        updatePoints();
-        countPercent.textContent = `You have x${totalPercent.toFixed(1)} percent multiplier, next upgrade costs ${costPercent.toFixed(1)}`;
+function buyResearcher(){
+    if (parsedScience >= parsedResearcherCost){
+        science.innerHTML = Math.round(parsedScience -= parsedResearcherCost)
+        researcherCount.innerHTML ++
+
+        parsedResearcherIncrease = parseFloat((researcherCount.innerHTML * 1).toFixed(2))
+        researcherIncrease.innerHTML = parsedResearcherIncrease
+        parsedResearcherCost *= 1.2;
+        researcherCost.innerHTML = Math.round(parsedResearcherCost)
+        sciencePerSecond = 0 + parsedResearcherIncrease
     }
-};
+}
 
-buttonAutoPoints.onclick = () => {
-    if(totalPoints>=costAutoPoints){
-        autoPointsMultiplier = (autoPointsMultiplier+.01)*1.1;
-        totalPoints -= costAutoPoints;
-        costAutoPoints = costAutoPoints*1.1;
-        updatePoints();
-        countAutoPoints.textContent = `You make ${(autoPoints*2).toFixed(1)} percentil Auto Points pe second , next upgrade costs ${costAutoPoints.toFixed(1)}`;
-    }
-};
-
-setInterval(() => {
-    totalPoints += autoPoints;
-    updatePoints();
-}, 500);
+setInterval(() =>{
+    parsedResources += resourcesPerSecond / 10
+    resources.innerHTML = Math.round(parsedResources)
+    parsedScience += sciencePerSecond / 10
+    science.innerHTML = Math.round(parsedScience)
+    rps.innerHTML = Math.round(resourcesPerSecond)
+    sps.innerHTML = Math.round(sciencePerSecond)
+    randspc.innerHTML = Math.round(resourcesPerClick)
+},100)
